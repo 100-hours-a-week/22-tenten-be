16e2dbaa06f5c40448664699d488f4ccad7e89ac
hyup98 <hyup98@naver.com>
2025-06-15 22:53:28 +0900

    [FEAT] Cache를 직렬화 해주는 PostCacheUitl생성

---
+++ src/main/java/com/kakaobase/snsapp/domain/posts/util/PostCacheUtil.java
@@ -0,0 +1,25 @@
+package com.kakaobase.snsapp.domain.posts.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakaobase.snsapp.global.common.redis.CacheRecord;
+import com.kakaobase.snsapp.global.common.redis.RedisHashUtil;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostCacheUtil extends RedisHashUtil<CacheRecord.PostStatsCache> {
+
+    public PostCacheUtil(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper) {
+        super(redisTemplate, objectMapper);
+    }
+
+    @Override
+    public void save(String key, CacheRecord.PostStatsCache value) {
+        putAll(key, value);
+    }
+
+    @Override
+    public CacheRecord.PostStatsCache load(String key) {
+        return getAll(key, CacheRecord.PostStatsCache.class);
+    }
+}
