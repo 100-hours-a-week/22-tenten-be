00c5a119bedbe7e54486604115e0ac58c9084ab8
hyup98 <hyup98@naver.com>
2025-06-20 22:55:54 +0900

    [COMMENT] PostCacheSyncScheduler주석 수정

--- src/main/java/com/kakaobase/snsapp/domain/posts/scheduler/PostCacheSyncScheduler.java
+++ src/main/java/com/kakaobase/snsapp/domain/posts/scheduler/PostCacheSyncScheduler.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 /**
- * 게시글 통계 캐시-DB 동기화 스케줄러
+ * 게시글 캐시데이터-DB 동기화 스케줄러
  * - 1분마다 실행
  * - Redisson 분산 락 사용
  * - 실제 동기화 로직은 PostCacheSyncService에 위임
