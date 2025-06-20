0da7af0b5a831d64c4ca52cd70a60f1c2b14d810
hyup98 <hyup98@naver.com>
2025-06-20 17:36:29 +0900

    [FEAT] 동기화 스케줄러 인터페이스에 동기화 목록 일괄 제거 추가

--- src/main/java/com/kakaobase/snsapp/global/common/redis/service/CacheSyncService.java
+++ src/main/java/com/kakaobase/snsapp/global/common/redis/service/CacheSyncService.java
@@ -1,11 +1,16 @@
 package com.kakaobase.snsapp.global.common.redis.service;
 
-//
-public interface CacheSyncService<V> {
+import java.util.List;
+
+
+public interface CacheSyncService<V> { //V는 동기화 목록 Set에 들어갈 값
+
     //동기화가 필요한 키값 목록 반환
     void syncCacheToDB();
-
+    //동기화목록에 값 추가
     void addToSyncList(V value);
-
+    //동기화 목록 단일 제거
     void removeFromSyncList(V value);
+    //동기화 목록 단체 제거
+    void removeFromSyncList(List<V> keys);
 }
