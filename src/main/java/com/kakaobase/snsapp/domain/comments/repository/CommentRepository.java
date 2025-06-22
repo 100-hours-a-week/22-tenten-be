a8ef3f55b175c1b33f7ad29a281859b323e63035
hyup98 <hyup98@naver.com>
2025-06-23 04:38:20 +0900

    [FEAT] PostId를 매개로 연관된 댓글을 일괄 삭제하는 메서드 추가

--- src/main/java/com/kakaobase/snsapp/domain/comments/repository/CommentRepository.java
+++ src/main/java/com/kakaobase/snsapp/domain/comments/repository/CommentRepository.java
@@ -20,10 +20,19 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
     boolean existsByIdAndMember_Id(Long id, Long memberId);
 
     /**
+     * 특정 게시글과 연관된 모든 댓글을 소프트 삭제 처리합니다.
+     * 게시글이 삭제될 때 해당 게시글의 모든 댓글을 함께 삭제합니다.
+     *
+     * @param postId 삭제할 게시글 ID
+     */
+    @Modifying
+    @Query("UPDATE Comment c SET c.deletedAt = CURRENT_TIMESTAMP WHERE c.post.id = :postId AND c.deletedAt IS NULL")
+    void deleteByPostId(@Param("postId") Long postId);
+
+    /**
      * 특정 댓글의 RecommentCount수를 1증가시키는 벌크 업데이트 메서드
      */
     @Modifying
     @Query("UPDATE Comment c SET c.recommentCount = c.recommentCount + 1 WHERE c.id = :commentId")
     int incrementRecommentCount(@Param("commentId") Long commentId);
-
 }
