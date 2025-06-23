1b007408c6f40fa82e3fe26af692932afdc8748a
hyup98 <hyup98@naver.com>
2025-06-23 17:47:31 +0900

    [REMOVE] RecommentLikeRepository의 과도한 주석 삭제

--- src/main/java/com/kakaobase/snsapp/domain/comments/repository/RecommentLikeRepository.java
+++ src/main/java/com/kakaobase/snsapp/domain/comments/repository/RecommentLikeRepository.java
@@ -19,10 +19,6 @@ public interface RecommentLikeRepository extends JpaRepository<RecommentLike, Re
 
     /**
      * 특정 회원이 특정 대댓글에 좋아요를 눌렀는지 확인합니다.
-     *
-     * @param memberId 회원 ID
-     * @param recommentId 대댓글 ID
-     * @return 좋아요 정보 (Optional)
      */
     @Query("SELECT rl FROM RecommentLike rl WHERE rl.member.id = :memberId AND rl.recomment.id = :recommentId")
     Optional<RecommentLike> findByMemberIdAndRecommentId(@Param("memberId") Long memberId, @Param("recommentId") Long recommentId);
