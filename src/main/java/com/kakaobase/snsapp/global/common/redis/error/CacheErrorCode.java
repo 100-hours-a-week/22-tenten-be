bda07dfd929d3931c7fec0daeda4fd2ce7c10412
hyup98 <hyup98@naver.com>
2025-06-23 23:58:47 +0900

    [FEAT] 캐시 필드 수정도중 발생하는 에러메세지의 변수명을 직관적으로 수정

--- src/main/java/com/kakaobase/snsapp/global/common/redis/error/CacheErrorCode.java
+++ src/main/java/com/kakaobase/snsapp/global/common/redis/error/CacheErrorCode.java
@@ -9,11 +9,10 @@ public enum CacheErrorCode{
 
     LOCK_ERROR("락 수행중 예외 발생"),
     LOCK_ACQUISITION_FAIL("락 수행중 예외 발생"),
-    CACHING_ERROR("캐시 생성중 에러 발생"),
+    CREATE_CACHE_ERROR("캐시 생성중 에러 발생"),
     CACHE_ALREADY_EXISTS("이미 존재하는 캐시에 생성 시도"),
     SYNC_ERROR("캐시 동기화 중 에러 발생"),
-    FIELD_UPDATE_ERROR("필드 업데이트 중 오류 발생"),
-    EX("그외");
+    FIELD_UPDATE_ERROR("필드 업데이트 중 오류 발생");
 
     private final String message;
 }
