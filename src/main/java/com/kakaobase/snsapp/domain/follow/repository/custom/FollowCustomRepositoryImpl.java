dfe705c7ef83420f418ac7adc91074cf4e21b6b5
hyup98 <hyup98@naver.com>
2025-06-24 00:24:16 +0900

    [FEAT] FollowCustomRepository에 회원 탈퇴시 사용할 특정 회원의 모든 팔로우, 팔로워 관계 정리 메서드 구현

--- src/main/java/com/kakaobase/snsapp/domain/follow/repository/custom/FollowCustomRepositoryImpl.java
+++ src/main/java/com/kakaobase/snsapp/domain/follow/repository/custom/FollowCustomRepositoryImpl.java
@@ -146,6 +146,90 @@ public class FollowCustomRepositoryImpl implements FollowCustomRepository {
                 .fetchOne();
     }
 
+    @Override
+    public void cleanupAllFollowRelationships(Long memberId) {
+        log.info("Starting cleanup of all follow relationships for member: {}", memberId);
+
+        // 1. 해당 유저를 팔로우한 사람들의 팔로잉 카운트 감소
+        Long decrementedFollowingCount = decrementFollowingCountForFollowersOf(memberId);
+        log.debug("Decremented following count for {} members", decrementedFollowingCount);
+
+        // 2. 해당 유저가 팔로우한 사람들의 팔로워 카운트 감소
+        Long decrementedFollowerCount = decrementFollowerCountForFollowingsOf(memberId);
+        log.debug("Decremented follower count for {} members", decrementedFollowerCount);
+
+        // 3. 해당 유저가 다른 사람들을 팔로우한 관계 삭제
+        Long deletedFollowingRelations = deleteByFollowerUserId(memberId);
+        log.debug("Deleted {} following relationships", deletedFollowingRelations);
+
+        // 4. 다른 사람들이 해당 유저를 팔로우한 관계 삭제
+        Long deletedFollowerRelations = deleteByFollowingUserId(memberId);
+        log.debug("Deleted {} follower relationships", deletedFollowerRelations);
+
+        log.info("Completed cleanup of all follow relationships for member: {} " +
+                        "(decremented: {} following, {} follower counts, deleted: {} following, {} follower relations)",
+                memberId, decrementedFollowingCount, decrementedFollowerCount,
+                deletedFollowingRelations, deletedFollowerRelations);
+    }
+
+    @Override
+    public Long deleteByFollowingUserId(Long followingUserId) {
+        log.info("Deleting all follow relationships where followingUserId = {}", followingUserId);
+
+        return queryFactory
+                .delete(follow)
+                .where(follow.followingUser.id.eq(followingUserId))
+                .execute();
+    }
+
+    @Override
+    public Long deleteByFollowerUserId(Long followerUserId) {
+        log.info("Deleting all follow relationships where followerUserId = {}", followerUserId);
+
+        return queryFactory
+                .delete(follow)
+                .where(follow.followerUser.id.eq(followerUserId))
+                .execute();
+    }
+
+    @Override
+    public Long decrementFollowingCountForFollowersOf(Long followingUserId) {
+        log.info("Decrementing followingCount for all followers of userId = {}", followingUserId);
+
+        return queryFactory
+                .update(QMember.member)
+                .set(QMember.member.followingCount, QMember.member.followingCount.subtract(1))
+                .where(
+                        QMember.member.id.in(
+                                queryFactory
+                                        .select(follow.followerUser.id)
+                                        .from(follow)
+                                        .where(follow.followingUser.id.eq(followingUserId))
+                        ),
+                        QMember.member.followingCount.gt(0) // 0보다 큰 경우에만 감소
+                )
+                .execute();
+    }
+
+    @Override
+    public Long decrementFollowerCountForFollowingsOf(Long followerUserId) {
+        log.info("Decrementing followerCount for all followings of userId = {}", followerUserId);
+
+        return queryFactory
+                .update(QMember.member)
+                .set(QMember.member.followerCount, QMember.member.followerCount.subtract(1))
+                .where(
+                        QMember.member.id.in(
+                                queryFactory
+                                        .select(follow.followingUser.id)
+                                        .from(follow)
+                                        .where(follow.followerUser.id.eq(followerUserId))
+                        ),
+                        QMember.member.followerCount.gt(0) // 0보다 큰 경우에만 감소
+                )
+                .execute();
+    }
+
     // === 헬퍼 메서드 ===
     private BooleanExpression cursorCondition(com.querydsl.core.types.dsl.NumberPath<Long> idPath, Long cursor) {
         return cursor != null ? idPath.gt(cursor) : null;
