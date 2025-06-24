0893dc2e4e52089d5d5f6ddd1ee9c218ed1a3128
1026hz <1026hzz@gmail.com>
2025-06-24 09:46:58 +0900

    [CHORE] CORS config 경로 사용하지 않는 것들 삭제 및 추가

--- src/main/java/com/kakaobase/snsapp/global/security/config/CorsConfig.java
+++ src/main/java/com/kakaobase/snsapp/global/security/config/CorsConfig.java
@@ -15,20 +15,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "https://localhost:3000",
-                "http://localhost:3001",
-                "https://localhost:3001",
-                "http://localhost:5500",
-                "http://localhost:8081",
-                "http://localhost:8080",
-                "http://172.16.24.221:8081",
-                "http://www.kakaobase.com",
                 "https://www.kakaobase.com",
-                "http://kakaobase.com",
-                "https://kakaobase.com",
-                "http://13.124.52.205:81",
-                "https://13.124.52.205:81"
+                "https://dev.kakaobase.com"
 
         ));
         config.addAllowedHeader("*");
