dddd005ceec7b86ddff2939694ae9c862bc0b1e6
hyup98 <hyup98@naver.com>
2025-06-30 00:57:40 +0900

    [FEAT] NotificationService 기본틀 구현

---
+++ src/main/java/com/kakaobase/snsapp/domain/notification/service/NotificationService.java
@@ -0,0 +1,9 @@
+package com.kakaobase.snsapp.domain.notification.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class NotificationService {
+}
