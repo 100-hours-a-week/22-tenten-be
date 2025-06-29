cbf22a5ce8582bd8318f22d6382c1545c231861c
hyup98 <hyup98@naver.com>
2025-06-30 00:57:23 +0900

    [FEAT] NotificationController 기본틀 구현

---
+++ src/main/java/com/kakaobase/snsapp/domain/notification/controller/NotificationController.java
@@ -0,0 +1,40 @@
+package com.kakaobase.snsapp.domain.notification.controller;
+
+import com.kakaobase.snsapp.domain.notification.dto.packets.NotificationNackPacket;
+import com.kakaobase.snsapp.global.common.entity.WebSocketPacket;
+import com.kakaobase.snsapp.global.error.code.ErrorPacketData;
+import com.kakaobase.snsapp.global.error.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class NotificationController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageExceptionHandler(Exception.class)
+    @SendToUser("/queue/errors")
+    public WebSocketPacket<ErrorPacketData> handleBusinessException(CustomException ex, Principal principal) {
+        log.warn("비즈니스 에러 - 사용자: {}, 에러: {}", principal.getName(), ex.getMessage());
+
+        var errorData = ErrorPacketData.builder()
+                .error("internal_server_error")
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return WebSocketPacket.<ErrorPacketData>builder()
+                .event("internal_server_error")
+                .data(errorData)
+                .build();
+    }
+}
