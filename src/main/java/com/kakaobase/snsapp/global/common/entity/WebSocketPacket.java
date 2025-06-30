f76c5f961ea98a69cc6a12e5c0c1a2536203f56a
hyup98 <hyup98@naver.com>
2025-06-30 20:37:52 +0900

    [FEAT] WebSocketPacket의 필드에 접근 가능하도록 public예약어 추가

--- src/main/java/com/kakaobase/snsapp/global/common/entity/WebSocketPacket.java
+++ src/main/java/com/kakaobase/snsapp/global/common/entity/WebSocketPacket.java
@@ -4,12 +4,11 @@ import lombok.Builder;
 
 @Builder
 public abstract class WebSocketPacket<T> {
-    String event;  // 세부 동작 (ex: liked, read, joined, error)
-    T data;       // 실제 메시지 데이터 (개별 DTO로 분기)
+    public final String event;  // 세부 동작 (ex: liked, read, joined, error)
+    public final T data;       // 실제 메시지 데이터 (개별 DTO로 분기)
 
     protected WebSocketPacket(String event, T data) {
         this.event = event;
         this.data = data;
     }
-
 }
