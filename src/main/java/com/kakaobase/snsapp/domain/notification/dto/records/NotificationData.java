e1c0584b02022fc1ba1d7e11933cd6c824a66580
hyup98 <hyup98@naver.com>
2025-06-30 20:37:08 +0900

    [REMOVE] NotificationData record에 message필드 삭제

--- src/main/java/com/kakaobase/snsapp/domain/notification/dto/records/NotificationData.java
+++ src/main/java/com/kakaobase/snsapp/domain/notification/dto/records/NotificationData.java
@@ -1,13 +1,17 @@
 package com.kakaobase.snsapp.domain.notification.dto.records;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kakaobase.snsapp.domain.members.dto.MemberResponseDto;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record NotificationData(
         Long id,
         MemberResponseDto.UserInfo sender,
         Long target_id,
-        String message,
+        String content,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime timestamp
 ) {}
