6c16ec96f53722d8e4c610ffdb6dfc55fcaeb8b1
hyup98 <hyup98@naver.com>
2025-06-30 20:36:37 +0900

    [FEAT] data record의 LocalDateTime에 JsonFormat으로 형식 일관화

--- src/main/java/com/kakaobase/snsapp/domain/notification/dto/records/NotificationNackData.java
+++ src/main/java/com/kakaobase/snsapp/domain/notification/dto/records/NotificationNackData.java
@@ -1,10 +1,13 @@
 package com.kakaobase.snsapp.domain.notification.dto.records;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record NotificationNackData(
         Long id,
         String error,
         String message,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime timestamp
 ) {}
