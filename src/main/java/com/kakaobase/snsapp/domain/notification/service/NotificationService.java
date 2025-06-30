b6cfd4747ed2bb85a16076db37f56d083a7366bb
hyup98 <hyup98@naver.com>
2025-06-30 21:21:44 +0900

    [FEAT] 외부 메서드에서 호출할 NotificationService구현

--- src/main/java/com/kakaobase/snsapp/domain/notification/service/NotificationService.java
+++ src/main/java/com/kakaobase/snsapp/domain/notification/service/NotificationService.java
@@ -1,9 +1,63 @@
 package com.kakaobase.snsapp.domain.notification.service;
 
+import com.kakaobase.snsapp.domain.members.dto.MemberResponseDto;
+import com.kakaobase.snsapp.domain.notification.dto.packets.NotificationRequestPacket;
+import com.kakaobase.snsapp.domain.notification.error.NotificationException;
+import com.kakaobase.snsapp.domain.notification.repository.NotificationRepository;
+import com.kakaobase.snsapp.domain.notification.util.NotificationType;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notifRepository;
+    private final NotificationCommandService commandService;
+
+
+    public void sendCommentCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfo userInfo) {
+        NotificationType type = NotificationType.COMMENT_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+    public void sendRecommentCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfo userInfo) {
+        NotificationType type = NotificationType.RECOMMENT_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+    public void sendPostLikeCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfo userInfo) {
+        NotificationType type = NotificationType.POST_LIKE_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+
+    public void sendCommentLikeCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfo userInfo) {
+        NotificationType type = NotificationType.COMMENT_LIKE_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+    public void sendRecommentLikeCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfo userInfo) {
+        NotificationType type = NotificationType.RECOMMENT_LIKE_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+    public void sendFollowingCreatedNotification(Long receiverId, Long postId, String content, MemberResponseDto.UserInfoWithFollowing userInfo) {
+        NotificationType type = NotificationType.FOLLOWING_CREATED;
+        Long notifId = commandService.createNotification(receiverId, type, postId);
+        commandService.sendNotification(notifId, type, content, postId, userInfo);
+    }
+
+
+    public void markNotificationRead(NotificationRequestPacket packet) {
+
+        commandService.updateNotificationRead(packet.data.id());
+    }
 }
