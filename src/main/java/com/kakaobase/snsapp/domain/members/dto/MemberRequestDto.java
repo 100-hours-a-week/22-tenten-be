b642744250216f803247b445271b8ba5f10e2c05
1026hz <1026hzz@gmail.com>
2025-07-04 14:12:31 +0900

    [CHORE] JsonProperty import

--- src/main/java/com/kakaobase/snsapp/domain/members/dto/MemberRequestDto.java
+++ src/main/java/com/kakaobase/snsapp/domain/members/dto/MemberRequestDto.java
@@ -2,6 +2,7 @@ package com.kakaobase.snsapp.domain.members.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * 회원 요청 DTO
