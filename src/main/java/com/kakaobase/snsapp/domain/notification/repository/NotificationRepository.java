10a2b7de2bfdb80882f90dedfa545aee979a80c4
hyup98 <hyup98@naver.com>
2025-07-06 18:47:57 +0900

    [REMOVE] 사용되지 않는 메서드 삭제

--- src/main/java/com/kakaobase/snsapp/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/kakaobase/snsapp/domain/notification/repository/NotificationRepository.java
@@ -5,15 +5,9 @@ import com.kakaobase.snsapp.domain.notification.repository.custom.CustomNotifica
 import com.kakaobase.snsapp.domain.notification.util.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, CustomNotificationRepository {
-
-    /**
-     * 알림 타입과 타겟 ID로 알림 목록 조회
-     */
-    List<Notification> findByNotificationTypeAndTargetId(NotificationType notificationType, Long targetId);
 
     /**
      * 알림 타입, 타겟 ID, 수신자 ID로 특정 알림 조회
